25fd54e56d0fa714d9d2ed3f295ecd5612823177
Cherenet <cherenetse@gmail.com>
2016-12-17 13:01:25 -0500

    loop through each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have names', function() {
+            for(i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+        });
     });
 
 
